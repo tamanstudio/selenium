e9b2122c329796e2cb462441300d670959d56a16
tamanstudio <tamankarya757@gmail.com>
2023-10-19 07:21:49 +0700

    login feature update

--- src/test/java/KasirAja/cucumber/features/login/login.feature
+++ src/test/java/KasirAja/cucumber/features/login/login.feature
@@ -1,13 +1,13 @@
 Feature: Log in to the application
 
   Scenario: Login to application as valid user
-    Given User launch the web app;
+    Given User launch the web app
     When  User enter the valid credentials
     And  Click on Login
     Then Home Page should be displayed
 
   Scenario: Login to application as Invalid user
-    Given User launch the web app;
+    Given User launch the web app
     When  User enter the inValid credentials
     And  Click on Login
     Then Error Message Should be displayed
--- src/test/java/KasirAja/cucumber/stepDef/login.java
+++ src/test/java/KasirAja/cucumber/stepDef/login.java
@@ -1,4 +1,63 @@
 package KasirAja.cucumber.stepDef;
 
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
 public class login {
+    WebDriver driver;
+    String baseurl = "https://kasirdemo.belajarqa.com/";
+
+    @Given("User launch the web app")
+    public void halaman_login_kasirAja(){
+        driver = new ChromeDriver();
+        driver.get(baseurl);
+        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+
+        //Assertion Login
+        String loginPageAssert = driver.findElement(By.xpath("//h2[contains(text(), 'hai, kasirAja')]")).getText();
+        Assert.assertEquals(loginPageAssert, "hai, kasirAja");
+    }
+
+    @When("User enter the valid credentials")
+    public void userEnterTheValidCredentials() {
+        //input Email
+        driver.findElement(By.id("email")).sendKeys("tdd-selenium@gmail.com");
+        //input password
+        driver.findElement(By.id("password")).sendKeys("tdd-selenium");
+    }
+
+    @And("Click on Login")
+    public void clickOnLogin() {
+        //click login
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+    }
+
+    @Then("Home Page should be displayed")
+    public void homePageShouldBeDisplayed() {
+        driver.findElement(By.xpath("//div[contains(text(), 'dashboard')]"));
+        String username = driver.findElement(By.xpath("//dd[contains(text(), 'hai']/preceding-sibling::dt")).getText();
+        Assert.assertEquals(username,"tdd-selenium");
+    }
+
+    @When("User enter the inValid credentials")
+    public void userEnterTheInValidCredentials() {
+        //input Email
+        driver.findElement(By.id("email")).sendKeys("tdd-selenium@gmail.com");
+        //input password
+        driver.findElement(By.id("password")).sendKeys("invalidpassword");
+    }
+
+    @Then("Error Message Should be displayed")
+    public void errorMessageShouldBeDisplayed() {
+        String ErrorLogin = driver.findElement(By.xpath("//div[@role='alert']")).getText();
+        Assert.assertEquals(ErrorLogin, "Kredensial yang Anda berikan salah");
+    }
 }
